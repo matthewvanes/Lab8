7787c41c3f331b8a433eaa071c438228ee2b43fd
Matthew Van Es <matthew.van.es@protonmail.com>
2020-03-13 13:35:00 -0700

    Initial Commit

---
+++ Lab8/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lab8
+//
+//  Created by MV X01a on 2020-03-13.
+//  Copyright © 2020 Matthew Van Es. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
